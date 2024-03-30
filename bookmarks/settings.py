d683f6e5d706e5959a09e3b802deb4db1be9204e
Omar Raafat <omar.raafat@bit68.com>
2024-03-30 03:37:04 +0200

    Extending the user model

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -119,6 +119,9 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 
+MEDIA_URL = 'media/'  # the base URL used to serve the media files uploaded by users
+MEDIA_ROOT = BASE_DIR / 'media'  # the local path where they reside
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
